064e11f5ed00aaf8409b4b9ea369176e17ec6732
auto_ommit <secretzqw@gmail.com>
2021-10-19 16:19:02 +0800

    jp 版本：10032710 hash: 9b21b8a430fcc0b33851e132702ce44f

--- jp/data/spskill_label_data.sql
+++ jp/data/spskill_label_data.sql
@@ -1,3 +1,3 @@
 CREATE TABLE 'spskill_label_data' ('unit_id' INTEGER NOT NULL, 'normal_label_text' TEXT NOT NULL, 'sp_label_text' TEXT NOT NULL, PRIMARY KEY('unit_id'));
 insert into spskill_label_data values (115801, '通常時', '変身中');
-insert into spskill_label_data values (118501, '通常時', 'RANK４以上時の特殊スキル');
+insert into spskill_label_data values (118501, '通常時', '特殊スキル');
