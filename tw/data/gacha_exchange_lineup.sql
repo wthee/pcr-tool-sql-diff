5fab2d8bf600e56ca21320e5f0402012a148d4ea
auto_ommit <secretzqw@gmail.com>
2022-02-11 16:41:57 +0800

    tw 版本：00050010 hash: 90dffe3606425d249349b9e630c7c114

--- tw/data/gacha_exchange_lineup.sql
+++ tw/data/gacha_exchange_lineup.sql
@@ -287,6 +287,7 @@ insert into gacha_exchange_lineup values (291, 156, 108101, 3, 0, '2022/01/26 16
 insert into gacha_exchange_lineup values (292, 156, 108301, 3, 0, '2022/01/26 16:00:00', '2022/02/01 15:59:59');
 insert into gacha_exchange_lineup values (293, 157, 110601, 3, 0, '2022/01/26 16:00:00', '2022/02/01 15:59:59');
 insert into gacha_exchange_lineup values (294, 158, 117701, 3, 1059, '2022/02/04 16:00:00', '2022/02/12 15:59:59');
+insert into gacha_exchange_lineup values (295, 159, 113901, 3, 0, '2022/02/12 16:00:00', '2022/02/19 15:59:59');
 insert into gacha_exchange_lineup values (298, 162, 106701, 3, 1061, '2022/02/01 16:00:00', '2022/02/04 15:59:59');
 insert into gacha_exchange_lineup values (299, 162, 180701, 3, 0, '2022/02/01 16:00:00', '2022/02/04 15:59:59');
 insert into gacha_exchange_lineup values (300, 162, 180301, 3, 0, '2022/02/01 16:00:00', '2022/02/04 15:59:59');
