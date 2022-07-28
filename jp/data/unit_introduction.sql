edffc3d55d473aee59df13a3a39ea74ca5f4d04b
auto_commit <secretzqw@gmail.com>
2022-07-28 10:52:26 +0800

    jp 版本：10040300 hash: d02bf6c5d5f82c3680af0d1a9378e7cf

--- jp/data/unit_introduction.sql
+++ jp/data/unit_introduction.sql
@@ -201,3 +201,4 @@ insert into unit_introduction values (199, 30206, 1, '2022/07/08 12:00:00', '202
 insert into unit_introduction values (200, 30207, 1, '2022/07/11 12:00:00', '2022/07/15 14:59:59', 380000, 380000, 0, 0, 0, 0);
 insert into unit_introduction values (201, 30208, 1, '2022/07/15 15:00:00', '2022/07/25 11:59:59', 380000, 380000, 0, 0, 0, 0);
 insert into unit_introduction values (202, 30209, 1, '2022/07/25 12:00:00', '2022/07/28 11:59:59', 380000, 380000, 0, 0, 0, 0);
+insert into unit_introduction values (203, 30210, 1, '2022/07/28 12:00:00', '2022/07/31 11:59:59', 380000, 380000, 0, 0, 0, 0);
