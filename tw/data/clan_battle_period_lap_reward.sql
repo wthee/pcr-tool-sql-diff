835fe777b882ae0e8795f79af2b97d73d26e0a43
auto_commit <secretzqw@gmail.com>
2023-02-20 14:32:24 +0800

    tw 版本：00110009 hash: cae15a96a8a65ec26b919852f9ea2774

--- tw/data/clan_battle_period_lap_reward.sql
+++ tw/data/clan_battle_period_lap_reward.sql
@@ -323,3 +323,8 @@ insert into clan_battle_period_lap_reward values (333, 1054, 1, 2, 2, 0, 12, 940
 insert into clan_battle_period_lap_reward values (334, 1054, 1, 3, 3, 0, 12, 94002, 500000, 2, 22002, 40, 2, 22003, 8, 0, 0, 0, 0, 0, 0);
 insert into clan_battle_period_lap_reward values (335, 1054, 1, 4, 4, 0, 12, 94002, 750000, 2, 22002, 60, 2, 22003, 12, 0, 0, 0, 0, 0, 0);
 insert into clan_battle_period_lap_reward values (336, 1054, 1, 5, -1, 0, 12, 94002, 1000000, 2, 22002, 80, 2, 22003, 16, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_lap_reward values (337, 1055, 1, 1, 1, 0, 12, 94002, 10000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_lap_reward values (338, 1055, 1, 2, 2, 0, 12, 94002, 250000, 2, 22002, 20, 2, 22003, 4, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_lap_reward values (339, 1055, 1, 3, 3, 0, 12, 94002, 500000, 2, 22002, 40, 2, 22003, 8, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_lap_reward values (340, 1055, 1, 4, 4, 0, 12, 94002, 750000, 2, 22002, 60, 2, 22003, 12, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_lap_reward values (341, 1055, 1, 5, -1, 0, 12, 94002, 1000000, 2, 22002, 80, 2, 22003, 16, 0, 0, 0, 0, 0, 0);
--- tw/data/clan_battle_period_rank_reward.sql
+++ tw/data/clan_battle_period_rank_reward.sql
@@ -750,3 +750,17 @@ insert into clan_battle_period_rank_reward values (748, 1054, 1, 3001, 5000, 0, 
 insert into clan_battle_period_rank_reward values (749, 1054, 1, 5001, 7000, 0, 8, 91002, 1500, 2, 90006, 1500, 2, 31018, 10, 0, 0, 0, 0, 0, 0);
 insert into clan_battle_period_rank_reward values (750, 1054, 1, 7001, 10000, 0, 8, 91002, 1000, 2, 90006, 1000, 2, 31018, 8, 0, 0, 0, 0, 0, 0);
 insert into clan_battle_period_rank_reward values (751, 1054, 1, 10001, -1, 0, 8, 91002, 750, 2, 90006, 750, 2, 31018, 7, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (752, 1055, 1, 1, 1, 0, 8, 91002, 20000, 2, 90006, 5000, 2, 31145, 30, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (753, 1055, 1, 2, 2, 0, 8, 91002, 15000, 2, 90006, 5000, 2, 31145, 30, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (754, 1055, 1, 3, 3, 0, 8, 91002, 12000, 2, 90006, 5000, 2, 31145, 30, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (755, 1055, 1, 4, 20, 0, 8, 91002, 10000, 2, 90006, 5000, 2, 31145, 30, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (756, 1055, 1, 21, 50, 0, 8, 91002, 8000, 2, 90006, 5000, 2, 31145, 30, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (757, 1055, 1, 51, 150, 0, 8, 91002, 6000, 2, 90006, 4500, 2, 31145, 25, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (758, 1055, 1, 151, 300, 0, 8, 91002, 4000, 2, 90006, 4000, 2, 31145, 20, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (759, 1055, 1, 301, 700, 0, 8, 91002, 3500, 2, 90006, 3500, 2, 31145, 18, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (760, 1055, 1, 701, 1500, 0, 8, 91002, 3000, 2, 90006, 3000, 2, 31145, 16, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (761, 1055, 1, 1501, 3000, 0, 8, 91002, 2500, 2, 90006, 2500, 2, 31145, 14, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (762, 1055, 1, 3001, 5000, 0, 8, 91002, 2000, 2, 90006, 2000, 2, 31145, 12, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (763, 1055, 1, 5001, 7000, 0, 8, 91002, 1500, 2, 90006, 1500, 2, 31145, 10, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (764, 1055, 1, 7001, 10000, 0, 8, 91002, 1000, 2, 90006, 1000, 2, 31145, 8, 0, 0, 0, 0, 0, 0);
+insert into clan_battle_period_rank_reward values (765, 1055, 1, 10001, -1, 0, 8, 91002, 750, 2, 90006, 750, 2, 31145, 7, 0, 0, 0, 0, 0, 0);
