80fcfddc68fa5ec740bc3a36b926943054875a6a
auto_commit <secretzqw@gmail.com>
2023-03-27 20:08:01 +0800

    cn 版本：202303231452 hash: 32dcbdbd5000410e6f8eaf53a8e89a5b

--- cn/data/special_story_banner.sql
+++ cn/data/special_story_banner.sql
@@ -1,2 +1,2 @@
 CREATE TABLE 'special_story_banner' ('id' INTEGER NOT NULL, 'story_group_id' INTEGER NOT NULL, 'start_time' TEXT NOT NULL, 'end_time' TEXT NOT NULL, PRIMARY KEY('id'));
-insert into special_story_banner values (1, 9002, '2021-01-30 05:00:00', '2021-02-28 11:59:59');
+insert into special_story_banner values (1, 9002, '2023/4/1 5:00:00', '2023/4/30 23:59:59');
