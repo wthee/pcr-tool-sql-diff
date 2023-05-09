5be107ca2635a50c22e36f99a637baeb67825de8
auto_commit <3116728602@qq.com>
2023-05-09 09:31:02 +0800

    tw 版本：00130001 hash: 2dbca774b94cc03db59b7f8708f0f03e

--- tw/data/room_item_get_announcement.sql
+++ tw/data/room_item_get_announcement.sql
@@ -12,3 +12,4 @@ insert into room_item_get_announcement values (10, 2816, '2023/02/15 05:00', '20
 insert into room_item_get_announcement values (11, 2818, '2022/07/01 16:00', '2030/12/31 23:59:59', '', '');
 insert into room_item_get_announcement values (12, 2805, '2022/09/23 16:00', '2030/12/31 23:59:59', '2022/09/23 16:00', '');
 insert into room_item_get_announcement values (13, 2828, '2023/02/01 16:00', '2030/12/31 23:59:59', '', '');
+insert into room_item_get_announcement values (14, 2829, '2023/05/09 16:00', '2030/12/31 23:59:59', '', '');
--- tw/data/room_setup.sql
+++ tw/data/room_setup.sql
@@ -353,3 +353,4 @@ insert into room_setup values (2818, 4, 6, 0);
 insert into room_setup values (2826, 3, 3, 0);
 insert into room_setup values (2827, 1, 1, 0);
 insert into room_setup values (2828, 4, 6, 0);
+insert into room_setup values (2829, 1, 2, 0);
