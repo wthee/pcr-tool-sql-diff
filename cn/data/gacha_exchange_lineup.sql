c8ad54dd61ccd4a94cdeecb293280a2fa8c906bb
auto_commit <3116728602@qq.com>
2023-07-14 20:08:02 +0800

    cn 版本：202307141904 hash: 78291721c467e819f3eaa3d132997f37

--- cn/data/gacha_exchange_lineup.sql
+++ cn/data/gacha_exchange_lineup.sql
@@ -255,3 +255,4 @@ insert into gacha_exchange_lineup values (256, 143, 107901, 3, 0, '2023/07/15 11
 insert into gacha_exchange_lineup values (257, 144, 117101, 3, 1052, '2023/07/15 11:00:00', '2023/07/24 10:59:59');
 insert into gacha_exchange_lineup values (258, 145, 110401, 3, 0, '2023/07/19 11:00:00', '2023/07/24 10:59:59');
 insert into gacha_exchange_lineup values (259, 146, 111801, 3, 0, '2023/07/24 11:00:00', '2023/07/31 10:59:59');
+insert into gacha_exchange_lineup values (260, 146, 115001, 3, 0, '2023/07/24 11:00:00', '2023/07/31 10:59:59');
