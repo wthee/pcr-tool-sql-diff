07d32dd6f7eb4a1f0e7bbd6d218fae0baebbf067
auto_commit <3116728602@qq.com>
2023-11-13 16:35:13 +0800

    tw 版本：00160033 hash: 638eef4229ef65e30c4c86415ff7ca64

--- tw/data/sqlite_stat1.sql
+++ tw/data/sqlite_stat1.sql
@@ -9,7 +9,7 @@ insert into sqlite_stat1 values ('goldset_data_2', None, '70');
 insert into sqlite_stat1 values ('emblem_mission_data', None, '899');
 insert into sqlite_stat1 values ('shiori_mission_reward_data', 'shiori_mission_reward_data_0_mission_reward_id', '1884 2');
 insert into sqlite_stat1 values ('shiori_item', None, '41');
-insert into sqlite_stat1 values ('campaign_schedule', None, '1532');
+insert into sqlite_stat1 values ('campaign_schedule', None, '1534');
 insert into sqlite_stat1 values ('tower_area_data', None, '49');
 insert into sqlite_stat1 values ('dsb_story_data', 'dsb_story_data_0_original_event_id', '9 9');
 insert into sqlite_stat1 values ('prizegacha_sp_detail', None, '5');
