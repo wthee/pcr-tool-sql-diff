5a947051293a05fc6cf2776bacfbd7d8cc2deb5d
auto_commit <3116728602@qq.com>
2024-02-15 12:54:59 +0800

    jp 版本：10053600 hash: 45165c89c2560eb7c76a8f235871cfc4

--- jp/data/legion_mission_category_data.sql
+++ jp/data/legion_mission_category_data.sql
@@ -1,6 +1,5 @@
 CREATE TABLE 'legion_mission_category_data' ('category_id' INTEGER NOT NULL, 'name' TEXT NOT NULL, PRIMARY KEY('category_id'));
 insert into legion_mission_category_data values (1000, '共通');
-insert into legion_mission_category_data values (1005, 'ミソラ');
-insert into legion_mission_category_data values (1006, 'ランファ');
-insert into legion_mission_category_data values (1007, 'アゾールド');
-insert into legion_mission_category_data values (1008, 'カリザ');
+insert into legion_mission_category_data values (1001, 'フェーズ1');
+insert into legion_mission_category_data values (1002, 'フェーズ2');
+insert into legion_mission_category_data values (1003, 'フェーズ3');
--- jp/data/sre_mission_category_data.sql
+++ jp/data/sre_mission_category_data.sql
@@ -1,6 +1,5 @@
 CREATE TABLE 'sre_mission_category_data' ('category_id' INTEGER NOT NULL, 'name' TEXT NOT NULL, PRIMARY KEY('category_id'));
 insert into sre_mission_category_data values (1000, '共通');
-insert into sre_mission_category_data values (1005, 'ミソラ');
-insert into sre_mission_category_data values (1006, 'ランファ');
-insert into sre_mission_category_data values (1007, 'アゾールド');
-insert into sre_mission_category_data values (1008, 'カリザ');
+insert into sre_mission_category_data values (1001, 'フェーズ1');
+insert into sre_mission_category_data values (1002, 'フェーズ2');
+insert into sre_mission_category_data values (1003, 'フェーズ3');
