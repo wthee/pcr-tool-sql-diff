e2a5a43e122e068f228cccedf64cb803edefd464
auto_commit <3116728602@qq.com>
2024-03-31 17:30:29 +0800

    tw 版本：00190010 hash: d1053f5ded37d0cae1e95d6eb2e67d35

--- tw/data/fbs_schedule.sql
+++ tw/data/fbs_schedule.sql
@@ -1,2 +1,8 @@
 CREATE TABLE 'fbs_schedule' ('fbs_id' INTEGER NOT NULL, 'story_id' INTEGER NOT NULL, 'start_time' TEXT NOT NULL, 'end_time' TEXT NOT NULL, PRIMARY KEY('fbs_id'));
 insert into fbs_schedule values (1, 4010001, '2024/03/31 05:00:00', '2024/04/01 04:59:59');
+insert into fbs_schedule values (2, 4010002, '2024/04/03 05:00:00', '2024/04/04 04:59:59');
+insert into fbs_schedule values (3, 4010003, '2024/04/05 05:00:00', '2024/04/06 04:59:59');
+insert into fbs_schedule values (4, 4010004, '2024/04/07 05:00:00', '2024/04/08 04:59:59');
+insert into fbs_schedule values (5, 4010005, '2024/04/10 05:00:00', '2024/04/11 04:59:59');
+insert into fbs_schedule values (6, 4010006, '2024/04/17 05:00:00', '2024/04/18 04:59:59');
+insert into fbs_schedule values (7, 4010007, '2024/04/29 05:00:00', '2024/04/30 04:59:59');
