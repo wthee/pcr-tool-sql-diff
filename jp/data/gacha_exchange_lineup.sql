5cec733442ebd7c7ba8fb26091988524561b6438
auto_commit <3116728602@qq.com>
2024-05-10 10:51:39 +0800

    jp 版本：10055400 hash: bd02b6fa13ea3041e18832989f99dda3

--- jp/data/gacha_exchange_lineup.sql
+++ jp/data/gacha_exchange_lineup.sql
@@ -766,3 +766,5 @@ insert into gacha_exchange_lineup values (764, 321, 107001, 3, 0, 0, '2024/04/30
 insert into gacha_exchange_lineup values (765, 321, 106101, 3, 0, 0, '2024/04/30 12:00:00', '2024/05/03 18:59:59');
 insert into gacha_exchange_lineup values (766, 321, 107101, 3, 0, 0, '2024/04/30 12:00:00', '2024/05/03 18:59:59');
 insert into gacha_exchange_lineup values (767, 322, 130701, 3, 1142, 30322, '2024/05/03 19:00:00', '2024/05/15 14:59:59');
+insert into gacha_exchange_lineup values (768, 323, 125501, 3, 0, 0, '2024/05/10 12:00:00', '2024/05/15 14:59:59');
+insert into gacha_exchange_lineup values (769, 323, 125601, 3, 0, 0, '2024/05/10 12:00:00', '2024/05/15 14:59:59');
