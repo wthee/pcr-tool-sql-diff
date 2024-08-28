129db40ec7803cc4078cee0d4ae3e42d9d4b2fb0
auto_commit <3116728602@qq.com>
2024-08-28 10:51:12 +0800

    jp_hash 版本：10058200 hash: bfc684304f57058bb58089b55f054d9e

--- jp_hash/data/v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49.sql
+++ jp_hash/data/v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49.sql
@@ -1,7 +1,7 @@
-CREATE TABLE 'v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49' ('115f9717e6f2734dccc8204e9194b313907b11c9021471ab3b9b244ea8c51143' TEXT NOT NULL, 'ac567aa75594fa1a08a2c51db5d0f3524a1b17937509f15e96719f4bbb0e0082' INTEGER NOT NULL, PRIMARY KEY('ac567aa75594fa1a08a2c51db5d0f3524a1b17937509f15e96719f4bbb0e0082'));
-insert into v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49 values ('バトル・スポーツ', 1);
-insert into v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49 values ('ことば・物語', 2);
-insert into v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49 values ('カルチャー・雑学', 3);
-insert into v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49 values ('サイエンス・地理・数学', 4);
-insert into v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49 values ('グルメ・ライフ', 5);
-insert into v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49 values ('人・生き物', 6);
+CREATE TABLE 'v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49' ('ac567aa75594fa1a08a2c51db5d0f3524a1b17937509f15e96719f4bbb0e0082' INTEGER NOT NULL, '115f9717e6f2734dccc8204e9194b313907b11c9021471ab3b9b244ea8c51143' TEXT NOT NULL, PRIMARY KEY('ac567aa75594fa1a08a2c51db5d0f3524a1b17937509f15e96719f4bbb0e0082'));
+insert into v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49 values (1, 'バトル・スポーツ');
+insert into v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49 values (2, 'ことば・物語');
+insert into v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49 values (3, 'カルチャー・雑学');
+insert into v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49 values (4, 'サイエンス・地理・数学');
+insert into v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49 values (5, 'グルメ・ライフ');
+insert into v1_f63178bfdbc338261334e9d6ce54b57adea677816bcd5181c3c53c5642ae6f49 values (6, '人・生き物');
